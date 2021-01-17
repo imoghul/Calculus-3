d4032acc08b3448b17d6eaf5059070dd65f92005
imoghul <moghul02@gmail.com>
2021-01-17 12:39:09 -0500

    operations for vectors

--- Vector.py
+++ Vector.py
@@ -29,16 +29,81 @@ class Vector:
 	def setZ(self,zcoor):
 		self.z=zcoor
 
-	def scaleNum(self,zcoor):
-		self.x*=num
-		self.y*=num
-		self.z*=num
 	
 	def scale(self,num):
-		self.x=self.cOperator.multiply(self.x,num)
-		self.y=self.cOperator.multiply(self.y,num)
-		self.z=self.cOperator.multiply(self.z,num)
+		if(type(num)==int):
+			if(type(self.x)==int):
+				self.x*=num
+			else:
+				self.x=self.cOperator.multiply(self.x,num)
+			if(type(self.y)==int):
+				self.y*=num
+			else:
+				self.y=self.cOperator.multiply(self.y,num)
+			if(type(self.z)==int):
+				self.z*=num
+			else:
+				self.z=self.cOperator.multiply(self.z,num)
+		else:
+			self.x=self.cOperator.multiply(self.x,num)
+			self.y=self.cOperator.multiply(self.y,num)
+			self.z=self.cOperator.multiply(self.z,num)
+	
+	def add(self,other):
+		if(type(self.x)==int and type(other.x)==int):
+			self.x += other.x
+		else:
+			self.x=self.cOperator.add(self.x,other.x)
+		if(type(self.y)==int and type(other.y)==int):
+    			self.y += other.y
+		else:
+			self.y=self.cOperator.add(self.y,other.y)
+		if(type(self.z)==int and type(other.z)==int):
+    			self.z += other.z
+		else:
+			self.z=self.cOperator.add(self.z,other.z)
+		
+	def subtract(self,other):
+		if(type(self.x)==int and type(other.x)==int):
+			self.x -= other.x
+		else:
+			self.x=self.cOperator.subtract(self.x,other.x)
+		if(type(self.y)==int and type(other.y)==int):
+			self.y -= other.y
+		else:
+			self.y=self.cOperator.subtract(self.y,other.y)
+		if(type(self.z)==int and type(other.z)==int):
+			self.z -= other.z
+		else:
+			self.z=self.cOperator.sub(self.z,other.z)
+	def multiply(self,other):
+		if(type(self.x)==int and type(other.x)==int):
+			self.x *= other.x
+		else:
+			self.x=self.cOperator.multiply(self.x,other.x)
+		if(type(self.y)==int and type(other.y)==int):
+			self.y *= other.y
+		else:
+			self.y=self.cOperator.multiply(self.y,other.y)
+		if(type(self.z)==int and type(other.z)==int):
+			self.z *= other.z
+		else:
+			self.z=self.cOperator.multiply(self.z,other.z)
 
+	def divide(self,other):
+		if(type(self.x)==int and type(other.x)==int):
+			self.x /= other.x
+		else:
+			self.x=self.cOperator.divide(self.x,other.x)
+		if(type(self.y)==int and type(other.y)==int):
+			self.y /= other.y
+		else:
+			self.y=self.cOperator.divide(self.y,other.y)
+		if(type(self.z)==int and type(other.z)==int):
+			self.z /= other.z
+		else:
+			self.z=self.cOperator.divide(self.z,other.z)
+			
 	def getMagnitude(self):
 		return self.cOperator.evaluate(self.cOperator.power( ( self.cOperator.addM([self.cOperator.multiply(self.x,self.x),self.cOperator.multiply(self.y,self.y),self.cOperator.multiply(self.z,self.z)])  ),.5))
 		
